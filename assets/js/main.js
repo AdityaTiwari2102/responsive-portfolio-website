b85b54eacb854cd2d66f50f4c393b7f47060b9f9
Aditya Tiwari <taditya.tiwari007@gmail.com>
2023-01-07 20:03:03 +0530

    Feat: Portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,17 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio__container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
